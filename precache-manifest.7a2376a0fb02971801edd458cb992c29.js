9a900b679c7c8c728934769c57d87ae7a53156e0
canzoroglu <canzoroglu@gmail.com>
2018-10-29 20:45:25 +0300

    Updates

---
+++ precache-manifest.7a2376a0fb02971801edd458cb992c29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f726a0ea515ef25c0f39",
+    "url": "/myreact/static/js/runtime~main.f726a0ea.js"
+  },
+  {
+    "revision": "0622dc1c22d19dcc358c",
+    "url": "/myreact/static/js/main.0622dc1c.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/myreact/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "0622dc1c22d19dcc358c",
+    "url": "/myreact/static/css/main.d02b7fb3.chunk.css"
+  },
+  {
+    "revision": "9958bdf1664454001f4d791aa296eae2",
+    "url": "/myreact/index.html"
+  }
+];
