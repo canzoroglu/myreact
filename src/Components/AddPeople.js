261881703882fc1d5484d9004bd7af4a72de4a50
canzoroglu <canzoroglu@gmail.com>
2018-12-12 10:07:42 +0300

    Make AddPeople functional component

--- src/Components/AddPeople.js
+++ src/Components/AddPeople.js
@@ -1,34 +1,32 @@
-import React, {Component} from "react";
+import React from "react";
 
-class AddPeople extends Component {
+function AddPeople(props) {
 
-  handleSubmit(e){
+  function handleSubmit(e){
     e.preventDefault();
     const inputs = document.querySelectorAll("input[type='text']");
     const name = document.querySelector("#name").value;
     const age = document.querySelector("#age").value;
     const profession = document.querySelector("#profession").value;
-    const people = this.props.people;
+    const people = props.people;
     let person = {name, age, profession, id: people[people.length - 1].id + 1};
-    this.props.onChange(person);
+    props.onChange(person);
     inputs.forEach(input => input.value = "");
     document.querySelector("#name").focus();
   }
-  render(){
-    return (
-      <div>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          <label htmlFor="name">Name: </label>
-          <input type="text" id="name" required autoFocus /><br />
-          <label htmlFor="age">Age: </label>
-          <input type="text" id="age" required /><br />
-          <label htmlFor="profession">Profession: </label>
-          <input type="text" id="profession" required /><br />
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" required autoFocus /><br />
+        <label htmlFor="age">Age: </label>
+        <input type="text" id="age" required /><br />
+        <label htmlFor="profession">Profession: </label>
+        <input type="text" id="profession" required /><br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddPeople;
