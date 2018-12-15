2988ca2093276c465bc9bcc351ba63e74f958433
canzoroglu <canzoroglu@gmail.com>
2018-12-15 16:41:00 +0300

    Add pokeball image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import Pokemon from "../assets/images/pokeball.png";
 
 class Home extends Component {
 
@@ -19,8 +20,9 @@ class Home extends Component {
     const postList = posts.length ? (posts.map(post => {
       return (
         <div className="post card" key={post.id}>
+          <img src={Pokemon} alt="A pokeball" />
           <div className="card-content">
-            <Link to={"/posts/" + post.id}><span className="card-title">{post.title}</span></Link>
+            <Link to={"/posts/" + post.id}><span className="card-title red-text">{post.title}</span></Link>
             <p>{post.body}</p>
           </div>
         </div>
@@ -30,7 +32,7 @@ class Home extends Component {
       No posts yet
     </div>);
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
