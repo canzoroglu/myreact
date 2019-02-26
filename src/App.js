24cec807eb6c49b3e3b91e20637b7f04f598cf5e
canzoroglu <canzoroglu@gmail.com>
2019-02-26 22:00:58 +0300

    Create folders and component files for initial set up

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
-import PeopleList from "./Components/PeopleList";
-import AddPeople from "./Components/AddPeople";
+import {BrowserRouter} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      people: [
-        {name: "Ali", age: 21, profession: "builder", id: 1},
-        {name: "John", age: 30, profession: "mechanic", id: 2},
-        {name: "Hans", age: 34, profession: "journalist", id: 3}
-      ]
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-  handleChange(person){
-    this.setState(state => {
-      state.people.push(person);
-      return state;
-    });
-  }
-  handleDelete(id){
-    const people = this.state.people.filter(person => person.id !== id);
-    this.setState({people,});
-  }
   render(){
     return (
-      <div>
-        <PeopleList onDelete={this.handleDelete} people={this.state.people} />
-        <AddPeople people={this.state.people} onChange={this.handleChange} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <h1>Start MarioPlan</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
