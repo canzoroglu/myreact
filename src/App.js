4550e0ce7289dfc719b896e45ea58c8506875cb8
canzoroglu <canzoroglu@gmail.com>
2019-02-27 15:42:50 +0300

    Complete lecture8

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
 import Dashboard from "./Components/dashboard/Dashboard";
 import ProjectDetails from "./Components/projects/ProjectDetails";
+import SignIn from "./Components/auth/SignIn";
+import SignUp from "./Components/auth/SignUp";
 import './App.css';
 
 class App extends Component {
@@ -14,6 +16,8 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
